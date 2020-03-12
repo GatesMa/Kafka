d386cd67228d8c01eb361398565fac466fb4afe6
GatesMa <gatesma@foxmail.com>
2020-03-12 20:05:34 +0800

    Kafka code

---
+++ kafka-learn/src/test/java/com/gatesma/kafkalearn/KafkaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatesma.kafkalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
